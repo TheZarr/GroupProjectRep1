547e36c29c49d52368817a3bcba636a685ce9c4c
Mogrady <mrogrady@att.net>
2018-07-07 11:14:25 -0400

    js update and some index changes

--- assets/Javascript/javascript.js
+++ assets/Javascript/javascript.js
@@ -1,59 +1,108 @@
-var destination = "";
-var startDate = "";
-var endDate = "";
+var destination = "Atlanta";
+var startDate = "20180901";
+var endDate = "20180905";
+var startDateFormatted = "20180901";
+var endDateFormatted = "20180905";
+var i = 10;
 
+$("#user-submit").on('click', function (event) {
+  event.preventDefault()
 
-$("#user-submit").on('click', function (event){
-event.preventDefault()
-console.log("user submission button works");
-//API Call for Eventful
-//api key for eventful = pXWvVZ6KKRfSxk4R
-//date format on eventful: &t=2018072100-2018072823 (YYYYMMDD-YYYYMMDD)
+  //grab user inputs and set local variables
+  destination = $("#travelDestinationInput").val().trim();
+  startDate = $("#startDateInput").val().trim();
+  endDate = $("#endDateInput").val().trim();
+  // console.log(destination);
+  // console.log(startDate);
+  // console.log(endDate);
 
-//grab user inputs and set local variables
-destination = $("#travelDestinationInput").val().trim();
-startDate = $("#startDateInput").val().trim();
-endDate = $("#endDateInput").val().trim();
+  startDateFormatted = moment(startDate).format("YYYYMMDD");
+  endDateFormatted = moment(endDate).format("YYYYMMDD");
+  // console.log(startDateFormatted);
+  // console.log(endDateFormatted);
+ 
+})
 
-//format dates
-var startDateFormatted = moment(startDate).format("YYYY MM DD");
-var endDateFormatted = moment(endDate).format("YYYY MM DD");
+eventCall();
+yelpCallHotel();
+yelpCallRest();
 
-//http://api.eventful.com/json/events/search?app_key=pXWvVZ6KKRfSxk4R&location=San+Diego&date=2018042500-2018042700
-var eventQueryURL = "http://atlanta.eventful.com/events?q=*&ga_search=" + destination + "*&sort_order=Date&within=25&units=mi&t=" + startDateFormatted + "-" + endDateFormatted + "&ga_type=events";
+function eventCall() {
 
-$.ajax({
+  //api key for eventful = pXWvVZ6KKRfSxk4R
+  //date format on eventful: &t=2018072100-2018072823 (YYYYMMDD-YYYYMMDD)
+
+  var eventQueryURL = "https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?app_key=pXWvVZ6KKRfSxk4R&location=" + destination + "&t=" + startDateFormatted + "00-" + endDateFormatted + "00";
+
+  $.ajax({
     url: eventQueryURL,
     method: "GET"
   })
-  .then(function(response) {
-    console.log(response);
-  });
+    .then(function (response) {
+      console.log(response);
+      console.log(response.events.event[0].postal_code);
+    }).catch(function (err) {
+      console.log(err);
+    });
+
+// for (var i = 0; i = 10; i++){
+
+var e = $("<p>");
+e.addClass("card-text");
+e.text("Hey");
+$("#events").append(e);
+// }
+}
 
 
-//API Call for Yelp
-//api key for yelp = w9W8FixtEsCBFVIOO3FGXPzz9cVEiOI7enTexK1ROsh5WHykbeEFJzovwpgELKVU0LdpfhC6RLKyNCa__Pqi9nXD0bVP90ZcVY2NU5La1t5yionbla1iImv9qP87W3Yx
+function yelpCallHotel() {
 
-var yelpQueryURL = "https://api.yelp.com/v3/businesses/search?term=restaurants&destination=" + destination + "&apikey=w9W8FixtEsCBFVIOO3FGXPzz9cVEiOI7enTexK1ROsh5WHykbeEFJzovwpgELKVU0LdpfhC6RLKyNCa__Pqi9nXD0bVP90ZcVY2NU5La1t5yionbla1iImv9qP87W3Yx";
-$.ajax({
-    url: yelpQueryURL,
-    method: "GET"
-  })
-  .then(function(response) {
-        console.log(response);
-  });
+  //API Call for Yelp
+  //api key for yelp = w9W8FixtEsCBFVIOO3FGXPzz9cVEiOI7enTexK1ROsh5WHykbeEFJzovwpgELKVU0LdpfhC6RLKyNCa__Pqi9nXD0bVP90ZcVY2NU5La1t5yionbla1iImv9qP87W3Yx
 
+  //api key for google places = AIzaSyD08e3WuFNHAIkG0DMXEYkaR2f1jaP-iOs
+  //https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Atlanta%20hotel&key=AIzaSyD08e3WuFNHAIkG0DMXEYkaR2f1jaP-iOs
 
-
-//API Call for Expedia
-
-// var expediaQueryURL = "";
-// $.ajax({
-//     url: expediaQueryURL,
-//     method: "GET"
-//   })
-//   .then(function(response) {
-
-//   });
+  var apiKeyYelp = "w9W8FixtEsCBFVIOO3FGXPzz9cVEiOI7enTexK1ROsh5WHykbeEFJzovwpgELKVU0LdpfhC6RLKyNCa__Pqi9nXD0bVP90ZcVY2NU5La1t5yionbla1iImv9qP87W3Yx"
   
-})
+  var hotelQueryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=hotels&location=Atlanta" //businesses/search?term=restaurants&destination=" + destination + "&API_KEY=w9W8FixtEsCBFVIOO3FGXPzz9cVEiOI7enTexK1ROsh5WHykbeEFJzovwpgELKVU0LdpfhC6RLKyNCa__Pqi9nXD0bVP90ZcVY2NU5La1t5yionbla1iImv9qP87W3Yx";
+  $.ajax({
+    method: "GET",
+    url: hotelQueryURL,
+    headers: {
+      "Authorization": "Bearer " + apiKeyYelp
+    }
+  })
+    .then(function (response) {
+      console.log(response);
+    }).catch(function (err) {
+      console.log(err);
+    });
+}
+
+function yelpCallRest() {
+
+  //API Call for Yelp
+  //api key for yelp = w9W8FixtEsCBFVIOO3FGXPzz9cVEiOI7enTexK1ROsh5WHykbeEFJzovwpgELKVU0LdpfhC6RLKyNCa__Pqi9nXD0bVP90ZcVY2NU5La1t5yionbla1iImv9qP87W3Yx
+
+  //api key for google places = AIzaSyD08e3WuFNHAIkG0DMXEYkaR2f1jaP-iOs
+  //https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Atlanta%20hotel&key=AIzaSyD08e3WuFNHAIkG0DMXEYkaR2f1jaP-iOs
+
+  var apiKeyYelp = "w9W8FixtEsCBFVIOO3FGXPzz9cVEiOI7enTexK1ROsh5WHykbeEFJzovwpgELKVU0LdpfhC6RLKyNCa__Pqi9nXD0bVP90ZcVY2NU5La1t5yionbla1iImv9qP87W3Yx"
+  
+  var restQueryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurants&location=Atlanta" //businesses/search?term=restaurants&destination=" + destination + "&API_KEY=w9W8FixtEsCBFVIOO3FGXPzz9cVEiOI7enTexK1ROsh5WHykbeEFJzovwpgELKVU0LdpfhC6RLKyNCa__Pqi9nXD0bVP90ZcVY2NU5La1t5yionbla1iImv9qP87W3Yx";
+  $.ajax({
+    method: "GET",
+    url: restQueryURL,
+    headers: {
+      "Authorization": "Bearer " + apiKeyYelp
+    }
+  })
+    .then(function (response2) {
+      console.log(response2);
+    }).catch(function (err) {
+      console.log(err);
+    });
+}
+
+
